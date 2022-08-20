d738b8ef91b0e820cda3cbaf8f98d5dad1b17469
Matheus <math.cs.ceil@gmail.com>
2022-08-20 09:34:53 -0300

    typeorm config

---
+++ src/database/migrations/1660998031743-User.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class User1660998031743 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
